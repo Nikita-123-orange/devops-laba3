76c19855d91c72e6aa46ecb1a0f5b6bad569d688
MulukovNikita <156002230+Nikita-123-orange@users.noreply.github.com>
2026-05-07 11:53:35 +0300

    fix docker-compose and init

--- vault/init/init-vault.sh
+++ vault/init/init-vault.sh
@@ -3,7 +3,9 @@
 export VAULT_ADDR=http://vault:8200
 export VAULT_TOKEN=root-token
 
-sleep 5
+echo "Waiting for Vault..."
+
+sleep 15
 
 vault secrets enable -path=secret kv-v2 || true
 
@@ -14,4 +16,4 @@ vault kv put secret/db \
     POSTGRES_HOST=db \
     POSTGRES_PORT=5432
 
-echo "Vault initialized successfully"
+echo "Vault initialized"
